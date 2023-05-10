56090ca31e40d2801b85adb974dfe6f7a347d6c6
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2023-05-11 00:16:07 +0400

    twick admin views

--- blog/models.py
+++ blog/models.py
@@ -4,13 +4,26 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 
 
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
 
-class Post(models.Models):
+    def __str__(self):
+        return self.full_name()
+
+
+class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=100)
@@ -18,4 +31,5 @@ class Post(models.Models):
     slug = models.SlugField(unique=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL, related_name="posts")
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
+    tags = models.ManyToManyField(Tag)
