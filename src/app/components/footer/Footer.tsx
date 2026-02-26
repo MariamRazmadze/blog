3260e899611c90aa734ca3a57aa913093ca3de8e
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2026-02-26 15:28:46 +0400

    add footer

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,5 +1,50 @@
+import Link from "next/link";
 import styles from "./footer.module.css";
+import Image from "next/image";
 
 export default function Footer() {
-  return <div className={styles.container}>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <div className={styles.logo}>
+          <Image src="/logo.svg" alt="Mariam's Blog" width={50} height={50} />
+          <h1 className={styles.logoText}>{"Mariam's Blog"}</h1>
+        </div>
+        <p className={styles.desc}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque
+          numquam, quidem alias eum, ipsam aliquam repudiandae voluptas optio
+          qui hic vero veniam ea quod ex atque asperiores natus facere sequi.
+        </p>
+        <div className={styles.icons}>
+          <Image src="/facebook.png" alt="" width={18} height={18} />
+          <Image src="/instagram.png" alt="" width={18} height={18} />
+          <Image src="/tiktok.png" alt="" width={18} height={18} />
+          <Image src="/youtube.png" alt="" width={18} height={18} />
+        </div>
+      </div>
+      <div className={styles.links}>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Links</span>
+          <Link href="/">Homepage</Link>
+          <Link href="/">Blog</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Tags</span>
+          <Link href="/">Coding</Link>
+          <Link href="/">Habits</Link>
+          <Link href="/">Health</Link>
+          <Link href="/">Meditation</Link>
+        </div>
+        <div className={styles.list}>
+          <span className={styles.listTitle}>Social</span>
+          <Link href="/">Facebook</Link>
+          <Link href="/">Instagram</Link>
+          <Link href="/">Tiktok</Link>
+          <Link href="/">Youtube</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
