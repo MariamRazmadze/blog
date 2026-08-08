bba84ae47fb7de252666244956ee7cfda8dc03f8
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2026-08-08 21:00:20 +0400

    add localeswitcher and active link styles

--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -1,5 +1,7 @@
+"use client";
+
 import styles from "./navbar.module.css";
-import { Link } from "@/i18n/navigation";
+import { Link, usePathname } from "@/i18n/navigation";
 import AuthLinks from "../authLinks/AuthLinks";
 import ThemeToggle from "../themeToggle/ThemeToggle";
 import { LuGithub } from "react-icons/lu";
@@ -7,9 +9,15 @@ import { LuLinkedin } from "react-icons/lu";
 import { LuYoutube } from "react-icons/lu";
 import { LuFacebook } from "react-icons/lu";
 import { useTranslations } from "next-intl";
+import LocaleSwitcher from "../localeSwitcher/LocaleSwitcher";
 
 export default function Navbar() {
   const t = useTranslations("default.nav");
+  const pathname = usePathname();
+  const isActive = (href: string) =>
+    href === "/" ? pathname === "/" : pathname.startsWith(href);
+  const navLinkClass = (href: string) =>
+    `${styles.link} ${isActive(href) ? styles.active : ""}`;
   return (
     <div className={styles.container}>
       <div className={styles.social}>
@@ -33,16 +41,17 @@ export default function Navbar() {
       <div className={styles.logo}> {t("title")}</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/" className={styles.link}>
+        <Link href="/" className={navLinkClass("/")}>
           {t("homepage")}
         </Link>
-        <Link href="/contact" className={styles.link}>
+        <Link href="/contact" className={navLinkClass("/contact")}>
           {t("contact")}
         </Link>
-        <Link href="/about" className={styles.link}>
+        <Link href="/about" className={navLinkClass("/about")}>
           {t("about")}
         </Link>
         <AuthLinks />
+        <LocaleSwitcher />
       </div>
     </div>
   );
