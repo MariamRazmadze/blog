10d0d1311646c836b4e5b7d9ded1186e5e3b926e
MariamRazmadze <mariam.razmadze.1@iliauni.edu.ge>
2026-08-10 15:28:18 +0400

    add some more strings

--- src/app/components/menu/Menu.tsx
+++ src/app/components/menu/Menu.tsx
@@ -1,18 +1,20 @@
 import styles from "./menu.module.css";
 import MenuPosts from "../menuPosts/MenuPosts";
 import MenuCategories from "../menuCategories/MenuCategories";
+import { useTranslations } from "next-intl";
 
 export default function Menu() {
+  const t = useTranslations("default.menu");
   return (
     <div className={styles.container}>
-      <h2 className={styles.subtitle}>{"What's hot?"}</h2>
-      <h1 className={styles.title}>Most Popular</h1>
+      <h2 className={styles.subtitle}>{t("hot")}</h2>
+      <h1 className={styles.title}>{t("popular")}</h1>
       <MenuPosts withImage={false} />
-      <h2 className={styles.subtitle}>Discover by topic</h2>
-      <h1 className={styles.title}>Categories</h1>
+      <h2 className={styles.subtitle}>{t("discoverByTopic")}</h2>
+      <h1 className={styles.title}>{t("categories")}</h1>
       <MenuCategories />
-      <h2 className={styles.subtitle}>Chosen by the editor</h2>
-      <h1 className={styles.title}>Editor Picks</h1>
+      <h2 className={styles.subtitle}>{t("handpicked")}</h2>
+      <h1 className={styles.title}>{t("worthReading")}</h1>
       <MenuPosts withImage={true} />
     </div>
   );
